c488cc4500dcac29c1a7264592f02294d4dac36c
austinntowns <59584876+austinntowns@users.noreply.github.com>
2020-02-03 21:13:48 -0700

    Setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAzcNjSOme45dUWqe-eV0aZw2dgdQCdXHQ",
+  authDomain: "authentication1-6be4d.firebaseapp.com",
+  databaseURL: "https://authentication1-6be4d.firebaseio.com",
+  projectId: "authentication1-6be4d",
+  storageBucket: "authentication1-6be4d.appspot.com",
+  messagingSenderId: "727755503950",
+  appId: "1:727755503950:web:9370ae0a73e1e4bf79c61f",
+  measurementId: "G-6YG3GRCGMR"
 };
 
 firebase.initializeApp(config);
